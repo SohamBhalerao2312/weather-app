d069a8a647a77099049c4d89c3220fee0ea1db7e
Soham Bhalerao <bhaleraosoham2003@gmail.com>
2024-03-26 23:29:19 +0530

    Complete Build

--- weather-app/ContentView.swift
+++ weather-app/ContentView.swift
@@ -8,52 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isNight=false
+    
     var body: some View {
         
         ZStack{
-            Backgroundview(topColor: .blue, bottomColor: .green)
+            Backgroundview(isNight: $isNight) //$sign is used coz this isNight variable is same as above one that we created using @State
             
             VStack{
                 CityTextView(cityname: "Pune", country: "India")
                 
-                VStack(spacing: 7){
-                    Image(systemName: "cloud.sun.fill")
-                        
-                        .resizable()
-                        .renderingMode(.original)
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width:180,height: 180)
-                        
-                    
-                    Text("35°C")
-                        .font(.system(size: 40 , weight: .medium,design: .default))
-                        .foregroundColor(.white)
-                        .padding(.horizontal, 9)
-                }
-                .padding(.bottom,32)
+                MainWeatherStatusView(imagename: isNight ?"moon.dust.fill" : "sun.haze.fill", temperature:isNight ? 19 : 32)
                 Spacer()
                 
                 HStack(spacing: 20){
-                    WeatherDayView(day_of_week: "TUE", img_name: "sun.haze.fill", temperature: 26)
-                    WeatherDayView(day_of_week: "WED", img_name: "cloud.sun.bolt.fill", temperature: 24)
-                    WeatherDayView(day_of_week: "THU", img_name: "cloud.moon.rain.fill", temperature: 21)
-                    WeatherDayView(day_of_week: "FRI", img_name: "cloud.sun.bolt.fill", temperature: 23)
-                    WeatherDayView(day_of_week: "SAT", img_name: "cloud.sun.fill", temperature: 26)
-                    WeatherDayView(day_of_week: "SUN", img_name: "sun.max.fill", temperature: 28)
+                    WeatherDayView(day_of_week: "TUE", img_name: isNight ? "moon.haze.fill" :"sun.haze.fill", temperature: 26)
+                    WeatherDayView(day_of_week: "WED", img_name:isNight ? "cloud.moon.bolt.fill" : "cloud.sun.bolt.fill", temperature: 24)
+                    WeatherDayView(day_of_week: "THU", img_name:isNight ? "wind" : "cloud.moon.rain.fill", temperature: 21)
+                    WeatherDayView(day_of_week: "FRI", img_name:isNight ? "smoke.fill" : "cloud.sun.bolt.fill", temperature: 23)
+                    WeatherDayView(day_of_week: "SAT", img_name:isNight ? "cloud.sleet.fill" : "cloud.sun.fill", temperature: 26)
+                    WeatherDayView(day_of_week: "SUN", img_name:isNight ? "moon.stars.fill" : "sun.max.fill", temperature: 28)
                 }
                 Spacer()
-                Button {
-                    //This part is to perform backend operations
-                    print("Tapped")
+                Button{
+                    //Performing Backend Operations
+                    isNight.toggle()
                 }label: {
                     Text("Change Day Time")
+                        .font(.system(size: 25,weight: .semibold,design: .default))
+                        .foregroundColor(isNight ? .blue : .white)
                         .frame(width: 280,height: 50)
-                        .foregroundColor(.white)
-                        .font(.system(size: 30, weight: .bold,design: .default))
-                        .background(Color.blue)
+                        .background(isNight ? .white : .blue)
                         .cornerRadius(10)
+                        
                 }
-                .padding(.bottom,32)
+                .padding(.bottom, 32)
                 
                 
             }
@@ -90,10 +80,11 @@ struct WeatherDayView: View {    //This is the extracted view made for code reus
 }
 
 struct Backgroundview: View {
-    var topColor:Color
-    var bottomColor:Color
+    
+    @Binding var isNight:Bool       //@Binding is used to add some extra functionality to the variable
+    
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [topColor,bottomColor]), startPoint: .topLeading, endPoint: .bottomTrailing)
+        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue , isNight ? .gray : .white  ]), startPoint: .topLeading, endPoint: .bottomTrailing)
             .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
@@ -108,3 +99,27 @@ struct CityTextView:View{
             .padding(.top, 40)
     }
 }
+
+struct MainWeatherStatusView: View {
+    var imagename:String
+    var temperature:Int
+    
+    var body: some View {
+        VStack(spacing: 20){
+            Image(systemName: imagename)
+            
+                .resizable()
+                .renderingMode(.original)
+                .aspectRatio(contentMode: .fit)
+                .frame(width:180,height: 180)
+            
+            
+            Text("\(temperature)°C")
+                .font(.system(size: 50 , weight: .medium,design: .default))
+                .foregroundColor(.white)
+                .padding(.horizontal, 9)
+        }
+        .padding(.bottom,32)
+    }
+}
+
